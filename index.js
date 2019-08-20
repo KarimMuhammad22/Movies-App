4afa39963b78117509062d2a135d346f0640632b
karim muhammad <benzema473@yahoo.com>
2019-08-20 02:39:04 +0200

    Working with MongoDB instead of Array of Genres

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
 const express = require("express");
 const app = express();
-const genres = require("./routes/genres");
+
+mongoose
+  .connect("mongodb://localhost/movies")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
 
 app.use(express.json());
-
 app.use("/api/genres", genres);
 
 const port = process.env.PORT || 3000;
